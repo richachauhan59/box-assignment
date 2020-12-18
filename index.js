031e14c320c83b87c267778bcab2e47a80517263
richachauhan59 <richachauhan2019@gmail.com>
2020-12-18 16:23:09 +0530

    Done with all box click functionality

---
+++ index.js
@@ -0,0 +1,54 @@
+
+var div1=document.querySelector('.children1')
+div1.addEventListener('click', ()=>{
+    if(div1.style.zIndex == 1){
+        div1.style.zIndex=2
+    }
+    else{
+        div1.style.zIndex=1
+    }
+    
+})
+var div2=document.querySelector('.children2')
+div2.addEventListener('click', ()=>{
+    if(div2.style.zIndex == 1){
+        div2.style.zIndex=2
+    }
+    else{
+        div2.style.zIndex=1
+    }
+    
+})
+
+var div3=document.querySelector('.children3')
+div3.addEventListener('click', ()=>{
+    if(div3.style.zIndex == 1){
+        div3.style.zIndex=2
+    }
+    else{
+        div3.style.zIndex=1
+    }
+    
+})
+
+var div4=document.querySelector('.children4')
+div4.addEventListener('click', ()=>{
+    if(div4.style.zIndex == 1){
+        div4.style.zIndex=2
+    }
+    else{
+        div4.style.zIndex=1
+    }
+    
+})
+
+var div5=document.querySelector('.children5')
+div5.addEventListener('click', ()=>{
+    if(div5.style.zIndex == 1){
+        div5.style.zIndex=2
+    }
+    else{
+        div5.style.zIndex=1
+    }
+    
+})
